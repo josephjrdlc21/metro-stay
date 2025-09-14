8893a11b9460872817b66eb5d922acd53f2dfb3f
Joseph Dela Cruz Jr <josephdelacruzjr4@gmail.com>
2025-09-14 17:45:54 +0800

    remove title in html portal

--- app/Laravel/Resources/views/portal.blade.php
+++ app/Laravel/Resources/views/portal.blade.php
@@ -4,7 +4,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Laravel</title>
         @viteReactRefresh
         @routes
         @vite('app/Laravel/Resources/js/portal/portal.jsx')
