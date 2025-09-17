449233254a399ccdf40cd92c0db27a735a3a37d5
Joseph Dela Cruz Jr. <josephdelacruzjr4@gmail.com>
2025-09-17 10:07:13 +0800

    ui adjustment on users index

--- app/Laravel/Resources/js/portal/modules/users/users-index.tsx
+++ app/Laravel/Resources/js/portal/modules/users/users-index.tsx
@@ -2,19 +2,29 @@ import MainLayout from "@portal/layouts/main-layout"
 import { Card } from "@chakra-ui/react"
 import { Heading } from "@chakra-ui/react"
 import { Breadcrumb } from "@chakra-ui/react"
-import { Flex } from "@chakra-ui/react"
-import { LuHouse } from "react-icons/lu"
+import { Flex, Separator } from "@chakra-ui/react"
+import { LuHouse, LuEllipsisVertical } from "react-icons/lu"
 import { Head } from "@inertiajs/react"
 import { Table } from "@chakra-ui/react"
 import { Status } from "@chakra-ui/react"
 import { Menu } from "@chakra-ui/react"
 import { Portal } from "@chakra-ui/react"
-import { IconButton } from "@chakra-ui/react"
-import { Box } from "@chakra-ui/react"
-import { LuEllipsisVertical } from "react-icons/lu"
+import { IconButton, Button } from "@chakra-ui/react"
+import { Box, HStack } from "@chakra-ui/react"
+import { Field, Input, Select, createListCollection } from "@chakra-ui/react"
+import { Grid } from "@chakra-ui/react"
+import { RiSearch2Line, RiResetRightLine, RiAddCircleLine } from "react-icons/ri"
+import { statusBadgeClass } from "@portal/utils/helper"
 
 export default function UsersIndex({ values }: { values: any }){
     //console.log(values);
+
+    const statuses = createListCollection({
+        items: [
+            { label: "Active", value: "active" },
+            { label: "Inactive", value: "inactive" },
+        ],
+    })
 
     return(
         <MainLayout>
@@ -47,8 +57,70 @@ export default function UsersIndex({ values }: { values: any }){
 
             <Card.Root size="sm" mt={5}>
                 <Card.Body>
+                    <Grid templateColumns={{ base: "repeat(1, 1fr)", lg: "repeat(4, 1fr)" }} gap={3} mb={3}>
+                        <Field.Root>
+                            <Field.Label>Search</Field.Label>
+                            <Input name="name" placeholder="e.g., Name, Email"/>
+                        </Field.Root>
+
+                        <Select.Root collection={statuses}>
+                            <Select.HiddenSelect />
+                            <Select.Label>Status</Select.Label>
+                            <Select.Control>
+                                <Select.Trigger>
+                                    <Select.ValueText placeholder="Select Status" />
+                                </Select.Trigger>
+                                <Select.IndicatorGroup>
+                                    <Select.Indicator />
+                                </Select.IndicatorGroup>
+                            </Select.Control>
+                            <Portal>
+                                <Select.Positioner>
+                                    <Select.Content>
+                                        {statuses.items.map((statuses) => (
+                                            <Select.Item item={statuses} key={statuses.value}>
+                                                {statuses.label}
+                                                <Select.ItemIndicator />
+                                            </Select.Item>
+                                        ))}
+                                    </Select.Content>
+                                </Select.Positioner>
+                            </Portal>
+                        </Select.Root>
+
+                        <Field.Root>
+                            <Field.Label>From</Field.Label>
+                            <Input type="date" name="from"/>
+                        </Field.Root>
+
+                        <Field.Root>
+                            <Field.Label>To</Field.Label>
+                            <Input type="date" name="to" />
+                        </Field.Root>
+                    </Grid>
+
+                    <HStack mb={2}>
+                        <Button colorPalette="cyan" variant="subtle" size="sm">
+                            <RiSearch2Line /> Filter
+                        </Button>
+                        <Button colorPalette="gray" variant="subtle" size="sm">
+                            <RiResetRightLine /> Clear
+                        </Button>
+                    </HStack>
+
+                    <Separator mt={2} mb={4}/>
+
+                    <Flex justify="space-between" align="center" mb={3}>
+                        <Heading _dark={{ color: "gray.300" }} color="gray.700" size="lg">User Records</Heading>
+                        <HStack>
+                            <Button colorPalette="cyan" variant="subtle" size="sm">
+                                <RiAddCircleLine /> Add User
+                            </Button>
+                        </HStack>
+                    </Flex>
+
                     <Box display="block" overflowX="auto" width="100%">
-                        <Table.Root>
+                        <Table.Root variant="outline">
                             <Table.Header>
                                 <Table.Row>
                                     <Table.ColumnHeader minW="200px">Name</Table.ColumnHeader>
@@ -63,7 +135,7 @@ export default function UsersIndex({ values }: { values: any }){
                                     <Table.Cell>Juan Dela Cruz</Table.Cell>
                                     <Table.Cell>Master Admin</Table.Cell>
                                     <Table.Cell>
-                                        <Status.Root colorPalette="green">
+                                        <Status.Root colorPalette={statusBadgeClass('active')}>
                                             <Status.Indicator />
                                             Active
                                         </Status.Root>
--- app/Laravel/Resources/js/portal/utils/helper.ts
+++ app/Laravel/Resources/js/portal/utils/helper.ts
@@ -5,3 +5,14 @@ export function strTitleCase(str: string): string {
         w.charAt(0).toUpperCase() + w.slice(1).toLowerCase()
     );
 }
+
+export function statusBadgeClass(status: string): string {
+    switch (status.toLowerCase()){
+        case "active":
+            return "green";
+        case "inactive":
+            return "red";
+        default:
+            return "gray";
+    }
+}
